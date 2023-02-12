301d988f239f3754b894c78c59d53b1dc74a2093
Adrian Rahmandanu <adrian.rahmandanu2@gmail.com>
2023-02-12 22:07:44 +0700

    add new

---
+++ database/migrations/2023_02_12_215241_add_asset_type_to_asset.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->foreignId('asset_type_id')->constrained('asset_type')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->dropForeign(['asset_type_id']);
+            $table->dropColumn('asset_type_id');
+        });
+    }
+};
